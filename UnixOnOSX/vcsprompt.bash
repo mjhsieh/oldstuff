222266317f1e8a034d466c2aaaf1edce87e12da0
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2009-02-11 08:55:45 +0000

    svk disabled

--- UnixOnOSX/vcsprompt.bash
+++ UnixOnOSX/vcsprompt.bash
@@ -29,16 +29,16 @@ __vcs_dir() {
     ref=$(svn info "$base_dir" | awk '/^URL/ { sub(".*/","",$0); r=$0 } /^Revision/ { sub("[^0-9]*","",$0); print r":"$0 }')
     vcs="svn"
   }
-  svk_dir() {
-    [ -f ~/.svk/config ] || return 1
-    base_dir=$(awk '/: *$/ { sub(/^ */,"",$0); sub(/: *$/,"",$0); if (match("'${PWD}'", $0"(/|$)")) { print $0; d=1; } } /depotpath/ && d == 1 { sub(".*/","",$0); r=$0 } /revision/ && d == 1 { print r ":" $2; exit 1 }' ~/.svk/config) && return 1
-    ref=${base_dir##*
-}
-    base_dir=${base_dir%%
-*}
-    sub_dir=$(sub_dir "${base_dir}")
-    vcs="svk"
-  }
+#  svk_dir() {
+#    [ -f ~/.svk/config ] || return 1
+#    base_dir=$(awk '/: *$/ { sub(/^ */,"",$0); sub(/: *$/,"",$0); if (match("'${PWD}'", $0"(/|$)")) { print $0; d=1; } } /depotpath/ && d == 1 { sub(".*/","",$0); r=$0 } /revision/ && d == 1 { print r ":" $2; exit 1 }' ~/.svk/config) && return 1
+#    ref=${base_dir##*
+#}
+#    base_dir=${base_dir%%
+#*}
+#    sub_dir=$(sub_dir "${base_dir}")
+#    vcs="svk"
+#  }
   hg_dir() {
     base_dir="."
     while [ ! -d "$base_dir/.hg" ]; do base_dir="$base_dir/.."; [ $(stat -f "${base_dir}") = "/" ] && return 1; done
@@ -49,21 +49,21 @@ __vcs_dir() {
   }
   git_dir ||
   svn_dir ||
-  svk_dir ||
+#  svk_dir ||
   hg_dir ||
   base_dir="$PWD"
 # echo "${vcs:+($vcs)}${_bold}${base_dir/$HOME/~}${_normal}${vcs:+\[$ref\]${_bold}${sub_dir}${_normal}}"
-if [ -n "$vcs" ]; then
-__vcs_prefix="($vcs)"
-__vcs_base_dir="${base_dir/$HOME/~}"
-__vcs_ref="[$ref]"
-__vcs_sub_dir="${sub_dir}"
-else
-__vcs_prefix=''
-__vcs_base_dir="${PWD/$HOME/~}"
-__vcs_ref=''
-__vcs_sub_dir=''
-fi
+  if [ -n "$vcs" ]; then
+    __vcs_prefix="($vcs)"
+    __vcs_base_dir="${base_dir/$HOME/~}"
+    __vcs_ref="[$ref]"
+    __vcs_sub_dir="${sub_dir}"
+  else
+    __vcs_prefix=''
+    __vcs_base_dir="${PWD/$HOME/~}"
+    __vcs_ref=''
+    __vcs_sub_dir=''
+  fi
 }
 PROMPT_COMMAND=__vcs_dir
 PS1='\u@\h:$__vcs_prefix\[${_bold}\]${__vcs_base_dir}\[${_normal}\]${__vcs_ref}\[${_bold}\]${__vcs_sub_dir}\[${_normal}\]\$ '
