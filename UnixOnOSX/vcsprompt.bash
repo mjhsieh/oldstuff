7f2cdd1c8820b51c2ce9d513106a2f487c0a9ab9
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2009-02-16 06:14:22 +0000

    1. portable readlink 2. cvs info, a start

--- UnixOnOSX/vcsprompt.bash
+++ UnixOnOSX/vcsprompt.bash
@@ -3,28 +3,49 @@
 # VCS
 _bold=$(tput bold)
 _normal=$(tput sgr0)
+_vcs_readlink() {
+  if [ -d $1 ]; then
+    (cd $1; pwd)
+  else
+    return 1
+  fi
+}
 __vcs_dir() {
   local vcs base_dir sub_dir ref
   sub_dir() {
     local sub_dir
-    sub_dir=$(stat -f "${PWD}")
+    sub_dir=$(_vcs_readlink "${PWD}")
     sub_dir=${sub_dir#$1}
     echo ${sub_dir#/}
   }
   git_dir() {
     base_dir=$(git rev-parse --show-cdup 2>/dev/null) || return 1
-    base_dir=$(stat -f "$base_dir/..")
+    base_dir=$(_vcs_readlink "$base_dir/..")
     sub_dir=$(git rev-parse --show-prefix)
     sub_dir=${sub_dir%/}
     ref=$(git symbolic-ref -q HEAD || git name-rev --name-only HEAD 2>/dev/null)
     ref=${ref#refs/heads/}
     vcs="git"
   }
+  cvs_dir() {
+    [ -d "CVS" ] && [ -d "CVS/Repository" ] || return 1
+    cvsbase="."
+    while [ -d "${cvsbase}/../CVS" ]; do
+      cvsbase="${cvsbase}/.."
+    done
+    cvsbase="$(_vcs_readlink ${cvsbase})"
+    cvsbranch="$(< CVS/Repository)"
+    rrn=${cvsbase##*/}
+    cvsbranch=${cvsbranch##${rrn}/}
+    [ -z ${cvsbranch} ] && cvsbranch=${rrn}
+    ref="${cvsbranch}:"
+    vcs="cvs"
+  }
   svn_dir() {
     [ -d ".svn" ] || return 1
     base_dir="."
     while [ -d "$base_dir/../.svn" ]; do base_dir="$base_dir/.."; done
-    base_dir=$(stat -f "$base_dir")
+    base_dir=$(_vcs_readlink "$base_dir")
     sub_dir=$(sub_dir "${base_dir}")
     ref=$(svn info "$base_dir" | awk '/^URL/ { sub(".*/","",$0); r=$0 } /^Revision/ { sub("[^0-9]*","",$0); print r":"$0 }')
     vcs="svn"
@@ -41,8 +62,8 @@ __vcs_dir() {
 #  }
   hg_dir() {
     base_dir="."
-    while [ ! -d "$base_dir/.hg" ]; do base_dir="$base_dir/.."; [ $(stat -f "${base_dir}") = "/" ] && return 1; done
-    base_dir=$(stat -f "$base_dir")
+    while [ ! -d "$base_dir/.hg" ]; do base_dir="$base_dir/.."; [ $(_vcs_readlink "${base_dir}") = "/" ] && return 1; done
+    base_dir=$(_vcs_readlink "$base_dir")
     sub_dir=$(sub_dir "${base_dir}")
     ref=$(< "${base_dir}/.hg/branch")
     vcs="hg"
@@ -51,6 +72,7 @@ __vcs_dir() {
   svn_dir ||
 #  svk_dir ||
   hg_dir ||
+  cvs_dir ||
   base_dir="$PWD"
 # echo "${vcs:+($vcs)}${_bold}${base_dir/$HOME/~}${_normal}${vcs:+\[$ref\]${_bold}${sub_dir}${_normal}}"
   if [ -n "$vcs" ]; then
