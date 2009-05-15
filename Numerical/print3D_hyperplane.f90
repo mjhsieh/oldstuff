b58974f106e48e8242bd2b6c66752d9770eff78a
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2009-05-15 00:21:23 +0000

    cosmetic change

--- Numerical/print3D_hyperplane.f90
+++ Numerical/print3D_hyperplane.f90
@@ -3,7 +3,7 @@ program print3DHyperplane
    integer xm,     ym,   zm
    integer xmymzm, xmym, nhpp
    integer ierr,   i
-   integer, allocatable :: hpporder(:), totprevhpp(:)
+   integer, allocatable :: hppseq(:), totprevhpp(:)
    xm      = 4
    ym      = 4
    zm      = 4
@@ -17,26 +17,26 @@ program print3DHyperplane
    ! | | | |
    ! +-+-+-+
 
-   allocate( hpporder(xmymzm),  stat = ierr )
+   allocate( hppseq(xmymzm),  stat = ierr )
    !   All numbered grids sorted with hyperplane ordering
    allocate( totprevhpp(nhpp+1),stat = ierr )
    !   The total grids of all previous hyperplanes
 
-   call hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hpporder,totprevhpp)
+   call hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hppseq,totprevhpp)
    do i = 1, nhpp
-      print *,hpporder(totprevhpp(i)+1:totprevhpp(i+1))
+      print *,hppseq(totprevhpp(i)+1:totprevhpp(i+1))
    enddo
 
-   deallocate( hpporder,  stat = ierr )
+   deallocate( hppseq,  stat = ierr )
    deallocate( totprevhpp,stat = ierr )
 end program
 
-subroutine hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hpporder,totprevhpp)
-!  This provides a 3-d hyperplane ordering sequence in hpporder.
+subroutine hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hppseq,totprevhpp)
+!  This provides a 3-d hyperplane ordering sequence in hppseq.
 !
 !  To use it, refer to this example:
 !       do i = 1, nhpp
-!          print *,hpporder(totprevhpp(i)+1:totprevhpp(i+1))
+!          print *,hppseq(totprevhpp(i)+1:totprevhpp(i+1))
 !       enddo
 !
 !  Inspired by Ushiro Yasunori's code in http://netlib.org/ddsv
@@ -45,10 +45,10 @@ subroutine hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hpporder,totprevhpp)
 
    integer xm,ym,zm,xmymzm,xmym,nhpp
    integer i,j,k,mygrid,ierr
-   integer hpporder(*),totprevhpp(*)
-   integer, allocatable :: whichhpp(:)
+   integer hppseq(*),totprevhpp(*)
+   integer, allocatable :: queryhpp(:)
    integer, allocatable :: lastinhpp(:)
-   allocate( whichhpp(xmymzm),stat = ierr )
+   allocate( queryhpp(xmymzm),stat = ierr )
    ! Which hyperplane does this grid belong to.
    allocate( lastinhpp(nhpp), stat = ierr )
    ! The last grid's rank in the hp ordering
@@ -56,31 +56,31 @@ subroutine hppidx(xm,ym,zm,xmymzm,xmym,nhpp,hpporder,totprevhpp)
    totprevhpp(1) = 0
    lastinhpp     = 0
 
-   ! H_m is the hyperplane
-   ! m = i + j + k
+   ! H_m is the hyperplane m start from 1
+   ! m = i + j + k - 2, 1 <= m <= xm+ym+zm-2
    do k =  0, zm-1
       do j = 0, ym-1
          mygrid=k*xmym+j*xm
          do i = 1, xm
-            whichhpp(mygrid+i)=i+j+k
+            queryhpp(mygrid+i)=i+j+k
          enddo 
       enddo
    enddo
    do i = 1, xmymzm
-      lastinhpp(whichhpp(i))=lastinhpp(whichhpp(i))+1
+      lastinhpp(queryhpp(i))=lastinhpp(queryhpp(i))+1
    enddo
    totprevhpp(2)=totprevhpp(1)+lastinhpp(1)
-   lastinhpp(1)=totprevhpp(1)
+   lastinhpp(1) =totprevhpp(1)
    do i = 2, nhpp
       totprevhpp(i+1)=totprevhpp(i)+lastinhpp(i)
-      lastinhpp(i)=totprevhpp(i)
+      lastinhpp(i)   =totprevhpp(i)
    enddo
    do i = 1, xmymzm
-      lastinhpp(whichhpp(i))=lastinhpp(whichhpp(i))+1
-      hpporder(lastinhpp(whichhpp(i)))=i
+      lastinhpp(queryhpp(i))=lastinhpp(queryhpp(i))+1
+      hppseq(lastinhpp(queryhpp(i)))=i
    enddo
 
-   deallocate( whichhpp, stat = ierr )
+   deallocate( queryhpp, stat = ierr )
    deallocate( lastinhpp,stat = ierr )
    return
 end subroutine hppidx
