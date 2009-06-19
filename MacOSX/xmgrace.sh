63697bbc383c0f6a74c00dbea995f09a43b98559
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2009-06-19 18:15:55 +0000

    fixed a bug of logical flaw

--- MacOSX/xmgrace.sh
+++ MacOSX/xmgrace.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+myMAXPATH=50000
 if [ "${1}test" != "test" ]; then
    cd "$(/usr/bin/dirname "$1")"
+   /opt/local/bin/xmgrace -g 1158x808 -maxpath ${myMAXPATH} "$1"
+else
+   /opt/local/bin/xmgrace -g 1158x808 -maxpath ${myMAXPATH}
 fi
-/opt/local/bin/xmgrace -g 1158x808 -maxpath 50000 "$1"
