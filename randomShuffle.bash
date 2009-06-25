322c3744d3cffe0c1bee66c3d2787425d033b34c
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2009-06-25 21:09:11 +0000

    added random seed

--- randomShuffle.bash
+++ randomShuffle.bash
@@ -1,6 +1,9 @@
-#****f* shadegrowncode/randomShuffle
 # NAME
 #   randomShuffle - prints random permutation of lines in standard input
+# ORIGINAL AUTHOR
+#   Nicholas Sushkin, Open Finance
+# MODIFICATION AUTHOR
+#   Mengjuei Hsieh, Univ. of California Irvine
 # FUNCTION
 #   Prints all or the first N lines of a random permutation of the
 #   lines of standard input. The random permutation is obtained using
@@ -11,26 +14,30 @@
 #   http://en.wikipedia.org/wiki/Knuth_shuffle
 # PARAMETERS
 #   * 1 - (optional) return only the first N of the random lines of input
+# USAGE
+#   echo $(printf "%s\n" 0 1 2 3 4 5 6 7 8 9 | randomShuffle)
 # SOURCE
 function randomShuffle
 {
+    SEED=$(head -1 /dev/urandom | od -N 1 | awk '{ print $2 }')
+    RANDOM=$SEED
     typeset -a elements
     typeset length=0
-    while read line
-    do
+
+    while read line; do
         elements[$length]=$line
         length=$(($length + 1))
     done
+
     typeset firstN=${1:-$length}
-    if [ $firstN -gt $length ]
-    then
+
+    if [ $firstN -gt $length ]; then
         firstN=$length
     fi
-    for ((i=0; $i < $firstN; i++))
-    do
+
+    for ((i=0; $i < $firstN; i++)); do
         randPos=$(($RANDOM % ($length - $i) ))
         printf "%s\n" "${elements[$randPos]}"
         elements[$randPos]=${elements[$length - $i - 1]}
     done
 }
-#***
