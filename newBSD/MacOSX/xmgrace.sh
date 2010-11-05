d3a80c5dbe14ef1ea0e510a51e15066ea7367519
mjhsieh <mjhsieh@b7949e2a-eb34-11dd-a971-7dbc132099af>
2010-11-05 07:46:14 +0000

    updated for XmgraceWrapper

--- newBSD/MacOSX/xmgrace.sh
+++ newBSD/MacOSX/xmgrace.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
 myMAXPATH=50000
 if [ "${1}test" != "test" ]; then
-   cd "$(/usr/bin/dirname "$1")"
-   /opt/local/bin/xmgrace -g 1158x808 -maxpath ${myMAXPATH} "$1"
+   myFILEPATH="$(/usr/bin/dirname "$1")"
+   if [ "${myFILEPATH}test" != "test" ]; then
+      cd "$myFILEPATH"
+   fi
+   /opt/local/bin/xmgrace -g 1130x894 -maxpath $myMAXPATH "$1"
 else
-   /opt/local/bin/xmgrace -g 1158x808 -maxpath ${myMAXPATH}
+   /opt/local/bin/xmgrace -g 1130x894 -maxpath $myMAXPATH
 fi
